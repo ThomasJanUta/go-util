628a018b29222e6b90be83612ec65121a846269b
Thomas Jan Uta <code@thomasuta.com>
2023-04-23 21:24:49 +0200

    refactor(workflow): rename logger

--- workflow/workflow.go
+++ workflow/workflow.go
@@ -12,35 +12,35 @@ import (
 
 type Workflow struct {
 	name    string
-	infoLog *log.Logger
+	logger  *log.Logger
 	start   time.Time
 	options interface{}
 }
 
-func New(workflowFunction interface{}, infoLog *log.Logger) *Workflow {
-	fullName := runtime.FuncForPC(reflect.ValueOf(workflowFunction).Pointer()).Name()
+func New(workflowFn interface{}, logger *log.Logger) *Workflow {
+	fullName := runtime.FuncForPC(reflect.ValueOf(workflowFn).Pointer()).Name()
 	parts := strings.Split(fullName, ".")
 	shortName := parts[len(parts)-1]
 	wf := Workflow{
-		name:    shortName,
-		infoLog: infoLog,
-		start:   time.Now(),
+		name:   shortName,
+		logger: logger,
+		start:  time.Now(),
 	}
 	return &wf
 }
 
 func (wf *Workflow) Start(args ...interface{}) {
-	wf.infoLog.Println("### WORKFLOW START:", wf.name, args)
+	wf.logger.Println("### WORKFLOW START:", wf.name, args)
 }
 func (wf *Workflow) Finish(args ...interface{}) {
 	duration := time.Since(wf.start).Minutes()
-	wf.infoLog.Println("### WORKFLOW FINISH:", wf.name,
+	wf.logger.Println("### WORKFLOW FINISH:", wf.name,
 		"| duration in minutes:", fmt.Sprintf("%.3f", duration),
 		args)
 }
 func (wf *Workflow) Step(args ...interface{}) {
-	wf.infoLog.Println("#####", wf.name, args)
+	wf.logger.Println("#####", wf.name, args)
 }
 func (wf *Workflow) Result(args ...interface{}) {
-	wf.infoLog.Println("### WORKFLOW RESULT:", wf.name, args)
+	wf.logger.Println("### WORKFLOW RESULT:", wf.name, args)
 }
